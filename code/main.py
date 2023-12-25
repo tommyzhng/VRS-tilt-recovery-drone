ca6fb32641289b446552f4a1bed24a840744350c
Tommy Zhang <tommyz2005@gmail.com>
2023-12-25 16:36:26 -0500

    update code

--- code/main.py
+++ code/main.py
@@ -1,18 +1,16 @@
 from drone import Drone
 from pymavlink import mavutil
+from commands import get_data
 import time
 
 drone = Drone()
-drone.set_mode("GUIDED")
-
-drone.arm_throttle()
-drone.takeoff(50)
+#drone.takeoff(50)
+#drone.send_acceleration() if drone.vehicle.altitude <= 49 else drone.send_acceleration(freefall=True)
 
 while True:
-    drone.vehicle.altitude = -drone.get_data("LOCAL_POSITION_NED", 20).get('z')
-    drone.vehicle.descent_rate = -drone.get_data("LOCAL_POSITION_NED", 20).get('vz')
-    drone.send_acceleration() if drone.vehicle.altitude <= 35 else drone.send_acceleration(freefall=True)
-    if drone.vehicle.altitude <= 2:
-       break
-    
+    drone.get_params()
+    drone.send_acceleration()
+    #if drone.vehicle.altitude <= 2:
+       #break
+
 drone.set_mode("LAND")
