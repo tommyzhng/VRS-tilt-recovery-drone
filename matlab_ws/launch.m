359f27105047305019a64e0b5b0ff6ab92f7b8bd
Tommy Zhang <tommyz2005@gmail.com>
2024-07-05 23:01:23 -0400

    update model with thrust normalization

--- matlab_ws/launch.m
+++ matlab_ws/launch.m
@@ -1,4 +1,4 @@
-m = 1.5;
+m = 1;
 R = 0.127;
 CDval = 0.008;
 Arm = [
@@ -8,7 +8,10 @@ Arm = [
 ];
 prop_inertia = (1/12) * 0.015 * 0.127^2;
 tilt_speed = [0; pi/4; 0];
-sample_time = 0.01;
+SampleTime = 0.01;
+
+m_to_mm = 1000;
 
 %% environmental variables
 g = 9.81;
+open('rotor.slx')
