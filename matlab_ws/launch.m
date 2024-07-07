37d8c3e22ba2d424d092a6ac2a0ad27c297952a9
Tommy Zhang <tommyz2005@gmail.com>
2024-07-06 20:23:53 -0400

    implement hitl in simulink

--- matlab_ws/launch.m
+++ matlab_ws/launch.m
@@ -1,17 +1,69 @@
+%% environmental variables
+g = 9.81;
+
+%% drone variables
 m = 1;
 R = 0.127;
 CDval = 0.008;
+CDDrone = 1.6e-7;
 Arm = [
 0.2*(1/sqrt(2)) -0.2*(1/sqrt(2)) 0.2*(1/sqrt(2)) -0.2*(1/sqrt(2))
 0.2*(1/sqrt(2)) -0.2*(1/sqrt(2)) -0.2*(1/sqrt(2)) 0.2*(1/sqrt(2))
 0 0 0 0
 ];
+Ixx = 0.005; %Kgm^2
+Iyy = 0.005;
+Izz = 0.009;
+
+inertia = diag([Ixx, Iyy, Izz]);
+
+rotorRPMMax = 10000;
+rotorTimeConst = 0.25;
 prop_inertia = (1/12) * 0.015 * 0.127^2;
 tilt_speed = [0; pi/4; 0];
-SampleTime = 0.01;
 
+% Contact model
+contact.translation.spring = 3100;
+contact.translation.damper = 100;
+contact.translation.friction = 0.5;
+contact.translation.vd = 0.02;
+contact.translation.maxFriction = 20;
+contact.translation.maxNormal = 80;
+
+contact.rotation.spring = 2;
+contact.rotation.damper = 1;
+contact.rotation.friction = 0.03;
+control.rotation.maxMoment = 0.1;
+control.rotation.friction = 0.025;
+control.rotation.vd = 0.2;
+
+%%initial states
+init.equilibriumZ =  m*g/contact.translation.spring;
+init.posNED = [0, 0, init.equilibriumZ]; % m
+init.vb = [0 0 0]'; %m/s
+init.euler = [0, 0, 0]'; %Roll Pitch Yaw Rads
+init.angRates = [0, 0, 0]; %rad/s
+ref_lat = 437818446e-7;
+ref_lon = -794679715e-7;
+ref_height = 76;
+
+%Gain to convert m to mm
 m_to_mm = 1000;
 
-%% environmental variables
-g = 9.81;
+%Gain to convert uT to Gauss
+uT_to_gauss = 0.01;
+
+%Gain to convert m/s^2 to mg
+ms2_to_mg = (1/9.80665)*1000;
+
+%Gain to convert m/s to cm/s
+ms_to_cms = 100;
+
+% Maximum Serial data read size from Pixhawk
+MAVLink_Input_Read_Size = 1024;
+
+% Sample Time of Plant and Controller (100 Hz)
+SampleTime = 0.01;
+
+
 open('rotor.slx')
